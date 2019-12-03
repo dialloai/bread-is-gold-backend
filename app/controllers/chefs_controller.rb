8e627e2ccf96ada856af8cbfbc880e0f8cfc83e1
github username <github email address>
2019-12-03 14:09:05 -0500

    chef controllers

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,2 +1,13 @@
 class ChefsController < ApplicationController
+
+  def index
+    chefs = Chefs.all
+    render json: chefs.to_json()
+  end
+
+  def show
+    chef = Chef.find(params[:id])
+    render json: chef.to_json()
+  end
+
 end
