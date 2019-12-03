39bbd989f83dc28273278aa8c1320c725bab0286
github username <github email address>
2019-12-03 16:19:08 -0500

    not sure if everything is working please check postges not working at the moment

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,8 @@
 class FavoritesController < ApplicationController
+
+  def index
+    favorites = Favorite.all
+    render json: favorites.to_json()
+  end
+
 end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,13 @@
 class RecipesController < ApplicationController
+
+  def index
+    recipes = Recipe.all
+    render json: recipes.to_json()
+  end
+
+  def show
+    recipe = Recipe.find(params[:id])
+    render json: recipe.to_json()
+  end
+
 end
